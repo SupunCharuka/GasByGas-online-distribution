2bebae597a68602502db9f0cfe132a5ae8a94542
SupunCharuka <supuncharuka.dev@gmail.com>
2025-01-13 13:24:33 +0530

    #1 Setup frontend

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>@yield('title') - Sailor Bootstrap Template</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Favicons -->
+    <link href="{{ asset('assets/frontend/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets/frontend//img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @include('frontend.layouts.styles')
+    @livewireStyles
+</head>
+
+<body class="index-page">
+    @include('frontend.layouts.header')
+
+    <main class="main">
+        @yield('content')
+    </main>
+
+    @include('frontend.layouts.footer')
+
+    <!-- Scroll Top -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
+    @include('frontend.layouts.scripts')
+    @livewireScripts
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Home page
+Route::get('', [PublicController::class, 'index'])->name('/');
+
+// Route::get('/', function () {
+//     return view('frontend.index')->name('/');
+// });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
