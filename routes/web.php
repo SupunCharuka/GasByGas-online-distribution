de692b8a5ba1f151ff0e39c116a487b8b3df1b4d
SupunCharuka <supuncharuka.dev@gmail.com>
2025-01-17 13:44:54 +0530

    #3 Dashboard design

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,8 @@ use Illuminate\Support\Facades\Route;
 // Home page
 Route::get('', [PublicController::class, 'index'])->name('/');
 
-// Route::get('/', function () {
-//     return view('frontend.index')->name('/');
-// });
-
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('backend.dashboard.index');
+    })->name('dashboard');
+});
